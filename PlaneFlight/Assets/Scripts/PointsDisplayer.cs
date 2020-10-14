d54fefc04f4522dddea16ba71195b0ecd126b207
Dziemo <michalek210@gmail.com>
2020-10-14 13:17:38 +0200

    Collecting stars

---
+++ PlaneFlight/Assets/Scripts/PointsDisplayer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PointsDisplayer : MonoBehaviour
+{
+    public IntRuntimeVariable variable;
+    public TextMeshProUGUI textDisplayer;
+
+    public void OnPointCollected ()
+    {
+        textDisplayer.text = variable.RuntimeValue.ToString();
+    }
+}
---
+++ PlaneFlight/Assets/Scripts/StarsController.cs
@@ -0,0 +1,21 @@
+﻿using ScriptableObjectArchitecture;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarsController : MonoBehaviour
+{
+    public GameEvent onStarCollected;
+    public IntRuntimeVariable playerStars;
+    public int starsToCollect = 1;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerStars.RuntimeValue += starsToCollect;
+            onStarCollected.Raise();
+            Destroy(gameObject);
+        }
+    }
+}
